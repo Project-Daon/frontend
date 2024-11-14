0555c9d7208ad8cf8387adf90893630c41d69857
wolpis <star@devksy.xyz>
2024-11-14 23:51:13 +0900

    update(main) : 로그인 로직 추가

--- src/pages/main/index.tsx
+++ src/pages/main/index.tsx
@@ -1,6 +1,27 @@
 import { MainCharacterComponent, MainDateComponent, MainDiaryComponent, MainHeaderComponent, MainTagsComponent } from "./components";
+import { useEffect } from "react"
+import { getCookie } from "../../utils/cookies"
+import axios from "axios"
+import { apihost } from "../../utils/host"
 
 const MainPage = () => {
+
+    useEffect(() => {
+        const token = getCookie("access_token");
+
+        axios.get(apihost + "/users/@me", {
+            headers: {
+                "Authorization Bearer": token
+            },
+            withCredentials: true
+        }).catch(() => {
+            location.replace("/login") // 200이 아니라면 로그인 페이지로 이동
+        }).then(() => {
+
+        })
+
+    }, [])
+
     return (
         <div>
             <MainHeaderComponent />
